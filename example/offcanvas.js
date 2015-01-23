4d4c54464b41048109d0b19766d8a33c17319b08
Caroline <cline.amaba@gmail.com>
2015-01-23 14:53:38 -0500

    groundworking

---
+++ example/offcanvas.js
@@ -0,0 +1,19 @@
+/* ========================================================================
+ * bootstrap.offcanvas: collapse.js v3.3.2
+ * http://github.com/clineamb/bootstrap.offcanvas
+ * ========================================================================
+ * Copyright 2015 Caroline Amaba
+ * Licensed under MIT
+ * ======================================================================== */
+
+ +function($) {
+    'use strict';
+
+    // OFFCANVAS PUBLIC CLASS DEFINITION
+    // =================================
+
+    var OffCanvas = function(element, options) {
+
+    };
+
+ }(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// --- bootstrap.offcanvas gulpfile
+
+var gulp    = require('gulp'),
+    less    = require('gulp-less'),
+    uglify  = require('gulp-uglify'),
+    rename  = require('gulp-rename')
+;
+
+gulp.task("build", ['less', 'less.min', 'js', 'js.min', 'example']);
+
+gulp.task('example', function() {
+    gulp.src([
+        "./dist/js/offcanvas.js",
+        "./bower_components/bootstrap/dist/css/bootstrap.min.css",
+        "./bower_components/bootstrap/dist/js/bootstrap.min.js"
+    ])  
+        .pipe(gulp.dest("./example"))
+    ;
+});
+
+gulp.task("less", function() {
+    gulp.src("./less/bootstrap.offcanvas.less")
+        .pipe(less({
+            paths: ["./less"]
+        }))
+        .pipe(gulp.dest("./example"))
+    ;
+
+    gulp.src("./less/bootstrap.offcanvas.less")
+        .pipe(less({
+            paths: ["./less"],
+            debug: true
+        }))
+        .pipe(rename({basename: "offcanvas"}))
+        .pipe(gulp.dest("./dist/css"))
+    ;
+});
+
+gulp.task("less.min", function() {
+    gulp.src("./less/bootstrap.offcanvas.less")
+        .pipe(less({
+            paths: ["./less"],
+            compress: true
+        }))
+        .pipe(rename({basename: "offcanvas", extname: ".min.css"}))
+        .pipe(gulp.dest("./dist/css"))
+    ;
+});
+
+gulp.task('js', function() {
+    gulp.src("./js/*.js")  
+        .pipe(gulp.dest("./dist/js"))
+    ;
+});
+
+gulp.task('js.min', function() {
+    gulp.src("./js/*.js")
+        .pipe(uglify())
+        .pipe(rename({extname: ".min.js"}))
+        .pipe(gulp.dest("./dist/js"))
+    ;
+});
